a047616c7b0a5fc90de7b3a064503519850f3236
Miacarmen <maitreyacarmen@gmail.com>
2024-04-05 00:40:55 -0700

    project pagecreated

---
+++ client/src/pages/ProjectPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+
+import '../styles/project.styles.css';
+import linkData from '../data/project-data.json';
+
+const ProjectPage = () => {
+  const { id } = useParams();
+  console.log(id);
+  const data = linkData.find((data) => data.id === parseInt(id));
+  console.log(data);
+  const { state } = useLocation(); // Use useLocation to access location state
+
+  return (
+    <div className='project-container'>
+      <>
+        <div className='project-content'>
+          <div>
+            <h1 className='p-title'>{data.title}</h1>
+            <p className='p-subtitle'>{data.subtitle}</p>
+          </div>
+        </div>
+        <div className='p-content-container'>
+          <div className='p-img-container'>
+            <img src={data.imgURL} alt={data.title}></img>
+          </div>
+          <div className='p-desc'>{data.desc}</div>
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default ProjectPage;
