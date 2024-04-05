797b99dc52d3c8d2ef69cbcebe565d7987a2d4ea
Miacarmen <maitreyacarmen@gmail.com>
2024-04-05 00:37:49 -0700

    app.js and index.js replaced, basic folders created

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+// Pages & Components
+import Home from './pages/Home';
+import About from './pages/About';
+import Work from './pages/Work';
+import ProjectPage from './pages/ProjectPage';
+import Contact from './pages/Contact';
+import NavBar from './components/Navigation/NavBar';
+import Footer from './components/Footer/Footer';
+import linkData from './data/project-data.json';
+
+// import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <NavBar />
+
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/work' element={<Work linkData={linkData} />} />
+          <Route path='/work/:id' element={<ProjectPage />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </>
   );
 }
 
