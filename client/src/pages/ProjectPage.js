b87b915973900b31af4c73178e4ca3c5c6df146a
Miacarmen <maitreyacarmen@gmail.com>
2024-11-08 13:26:18 -0800

    font style changed, hover button color change, scroll arrow added

--- client/src/pages/ProjectPage.js
+++ client/src/pages/ProjectPage.js
@@ -1,14 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { BsArrowDownShort } from 'react-icons/bs';
 
 import '../styles/project.styles.css';
 import linkData from '../data/project-data.json';
+
 
 const ProjectPage = () => {
   // const { id } = useParams();
   // const data = linkData.find((data) => data.id === parseInt(id));
   const { id } = useParams();
   const [data, setData] = useState(null);
+
+  const [showScrollIndicator, setShowScrollIndicator] = useState(true);
+
+  useEffect(() => {
+    const handleWheel = () => {
+      setShowScrollIndicator(false); // Hide the arrow when scrolling starts
+      window.removeEventListener('wheel', handleWheel);
+    };
+
+    window.addEventListener('wheel', handleWheel);
+
+    return () => {
+      window.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
 
   useEffect(() => {
     console.log('Fetching data for id:', id);
@@ -27,6 +44,11 @@ const ProjectPage = () => {
             <h1 className='p-title'>{data.title}</h1>
             <p className='p-subtitle'>{data.subtitle}</p>
           </div>
+          {showScrollIndicator && (
+                <div className='col-3-4' id='scrollIndicator'>
+                  <BsArrowDownShort style={{ width: '28px', height: '28px' }} />
+                </div>
+              )}
         </div>
         <div className='p-content-container'>
           {data.images.map((image, index) => (
@@ -40,7 +62,7 @@ const ProjectPage = () => {
           ))}
           <div className='p-desc'>{data.desc}</div>
           <div className='p-tools-container'>
-          <h3>Tools Used:</h3>
+          <h3 className='tools-header'>Tools Used:</h3>
           <div className='p-tools'>
             {data.tools.map((tool, index) => (
               <div key={index}>{tool}</div>
